5035113e512c6e836270d3978ae921fa97d534d3
lukaszsienko <l.sienko@stud.elka.pw.edu.pl>
2018-06-14 15:45:24 +0200

    git add lacking file

---
+++ src/pl/edu/pw/elka/phrasalwrapper/model_persistence/ModelsPersistence.java
@@ -0,0 +1,81 @@
+package pl.edu.pw.elka.phrasalwrapper.model_persistence;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ModelsPersistence {
+
+    public static ModelsPersistence createEmptyModelsDirectory(String pathToModelsDirectoryParent, String modelsDirectoryName) throws IOException {
+        File parentDirectory = new File(pathToModelsDirectoryParent);
+        if (!parentDirectory.exists() || !parentDirectory.isDirectory()) {
+            throw new IOException("Cannot create empty model directory. Specified parent path is wrong or leads to a file instead of a directory.");
+        }
+
+        File modelsDirectory = new File(parentDirectory, modelsDirectoryName);
+        boolean dirCreated = modelsDirectory.mkdir();
+        if (!dirCreated) {
+            throw new IOException("Cannot create new directory.\nDir name: "+modelsDirectoryName+"\nDir location: "+parentDirectory.getCanonicalPath());
+        }
+
+        ModelsPersistence modelsPersistence = new ModelsPersistence(modelsDirectory.getCanonicalPath());
+        return modelsPersistence;
+    }
+
+    public static ModelsPersistence loadModels(String pathToPersistedModelsDirectory) throws IOException {
+        File modelsDirectory = new File(pathToPersistedModelsDirectory);
+        if (!modelsDirectory.exists() || !modelsDirectory.isDirectory()) {
+            throw new IOException("Cannot open model directory. Specified directory does not exist or path leads to a file.");
+        }
+
+        ModelsPersistence modelsPersistence = new ModelsPersistence(modelsDirectory.getCanonicalPath());
+        modelsPersistence.detectExistingModelFiles();
+        if (modelsPersistence.checkNoModelFilesWereDetected()) {
+            throw new IOException("Cannot detect any model in specified model directory.");
+        }
+
+        return modelsPersistence;
+    }
+
+    private String canonicalPathToModelsDir;
+    private Map<ModelFile, String> canonicalPathsToDetectedModelFiles;
+
+    private ModelsPersistence(String canonicalPathToModelsDir) {
+        this.canonicalPathToModelsDir = canonicalPathToModelsDir;
+        this.canonicalPathsToDetectedModelFiles = new LinkedHashMap<>();
+    }
+
+    public String getCanonicalPathToModelsDir() {
+        return canonicalPathToModelsDir;
+    }
+
+    public void registerNewDetectedModelFile(ModelFile modelFile, String canonicalPath) {
+        canonicalPathsToDetectedModelFiles.put(modelFile, canonicalPath);
+    }
+
+    public String getDetectedModelFilePath(ModelFile modelFile) throws IOException {
+        String path = canonicalPathsToDetectedModelFiles.get(modelFile);
+        if (path == null || path.isEmpty()) {
+            throw new IOException("Specified model file wasn't detected. File: " + ModelFile.generateCanonicalPathToOneModelFile(this, modelFile));
+        }
+        return path;
+    }
+
+    private void detectExistingModelFiles() {
+        List<ModelFile> allModelFiles = Arrays.asList(ModelFile.values());
+        for (ModelFile modelFile: allModelFiles) {
+            String pathToFile = ModelFile.generateCanonicalPathToOneModelFile(this, modelFile);
+            File file = new File(pathToFile);
+            if (file.exists() && !file.isDirectory()) {
+                registerNewDetectedModelFile(modelFile, pathToFile);
+            }
+        }
+    }
+
+    private boolean checkNoModelFilesWereDetected() {
+        return canonicalPathsToDetectedModelFiles.isEmpty();
+    }
+}
